1183f5c401c28fbf6ab033dbeb0294117e205ed7
Shreedevi410 <shreedevi.v.nayak@gmail.com>
2026-07-01 13:07:06 +0530

    routerlink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { EmptyComponent } from './empty/empty';
+import { Home } from './home/home';
+import { SellerAuth } from './seller-auth/seller-auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: EmptyComponent
+  },
+  {
+    path: 'home',
+    component: Home
+  },
+  {
+    path: 'seller-auth',
+    component: SellerAuth
+  }
+];
