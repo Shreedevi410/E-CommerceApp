75e4467132afa6cacd51755dcde5c044e7926f5c
Shreedevi410 <shreedevi.v.nayak@gmail.com>
2026-07-06 13:18:24 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { EmptyComponent } from './empty/empty';
 import { Home } from './home/home';
 import { SellerAuth } from './seller-auth/seller-auth';
+import { SellerHome } from './seller-home/seller-home';
+import { authGuard } from '../auth-guard';
 
 export const routes: Routes = [
   {
@@ -16,5 +18,10 @@ export const routes: Routes = [
   {
     path: 'seller-auth',
     component: SellerAuth
+  },
+  {
+    path: 'seller-home',
+    component: SellerHome,
+    canActivate: [authGuard]
   }
 ];
